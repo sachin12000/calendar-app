cda94f33bcb1673ff62968886574b70981184bf3
sachin12000 <wills4705@gmail.com>
2023-11-17 04:50:47 -0500

    prints a message showing the build mode

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,12 @@ const developmentConfig = require('./webpack.dev');
 const productionConfig = require('./webpack.prod');
 
 module.exports = (env, args) => {
-    console.log(args.mode)
     switch (args.mode) {
         case 'development':
+            console.log("Running development build");
             return merge(commonConfig('development'), developmentConfig);
         case 'production':
+            console.log("Running production build");
             return merge(commonConfig('production'), productionConfig);
         default:
             throw new Error('No matching configuration was found!');
