be82f25add3e8f3c767a1cb825271387a9b8ea12
sachin12000 <wills4705@gmail.com>
2023-11-25 18:39:04 -0500

    added Montserrat Thin font

--- webpack.common.js
+++ webpack.common.js
@@ -47,6 +47,10 @@ module.exports = (mode) => {
                 {
                     test: /\.(png|svg|jpg|jpeg|gif)$/i,
                     type: 'asset/resource',
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                    type: 'asset/resource',
                 }
             ],
         },
